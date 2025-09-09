23b847f85c7669d2ce307231eb67600d5aab4180
InloranReliza <inloranreliza@gmail.com>
2025-09-09 16:34:18 +0800

    Added operators for daily routine

--- BacalandoLife.js
+++ BacalandoLife.js
@@ -6,4 +6,13 @@ let yearlevel = "2nd Year";
 
 console.log ("Student Name: "+ student + "\nCourse: " + course + "\nYear Level:"+ yearlevel + "\n");
 
+ let hoursOfSleep = 8;
+ let hoursOfStudy = 2;
+
+ let totalHours = hoursOfSleep + hoursOfStudy;
+
+    console.log("Hours of Sleep: " + hoursOfSleep + " hours");
+    console.log("Hours of Study: " + hoursOfStudy + " hours");
+    console.log("Total Hours of Sleep and Study: " + totalHours + " hours");
+
 
