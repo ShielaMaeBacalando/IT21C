c05ecaec5f5356667b041133d0914d5b1ac7716f
InloranReliza <inloranreliza@gmail.com>
2025-09-09 14:41:30 +0800

    Added Student Variables

---
+++ index.js
@@ -0,0 +1,20 @@
+console.log("Hello World!");
+
+let student = "Shiela Mae Bacalando";
+let grade = 1.0;
+let isPassed = true;
+let address;
+let partner = null;
+
+console.log ("Student Name: "+student + "\Final Grade: " + grade + "\isPassed?: " + isPassed + "\nAddress: " + address + "\nPartner: " + partner + "\n");
+
+let num1 = 12;
+let num2 = 11;
+
+let sum = num1+num2;
+let diff = num1-num2;
+let prod = num1*num2;
+let quo = num1/num2;
+let mod = num1%num2;
+
+console.log ("Sum: " + sum + "\nDifference: " + diff + "\inProduct?: " + prod + "\nQuotient: " + quo + "\nReminder: " + mod + "\n");
