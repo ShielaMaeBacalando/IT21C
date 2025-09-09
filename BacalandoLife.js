c43fc3e9869b1bc06268b398a72aaa4b6ec9ef92
InloranReliza <inloranreliza@gmail.com>
2025-09-09 10:27:08 +0800

    Added Student Variables

---
+++ BacalandoLife.js
@@ -0,0 +1,8 @@
+console.log("IT21C");
+
+let student = "Shiela Mae Bacalando";
+let course = "Bachelor of Science in Information Studies  ";
+let yearlevel = "2nd Year";
+
+
+console.log ("Student Name: "+ student + "\nCourse: " + course + "\nYear Level:"+ yearlevel + "\n");
